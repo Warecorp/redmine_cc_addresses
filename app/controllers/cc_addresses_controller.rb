c20e4f5d7b69a7fa500b4d5be89ac47e42a391c9
Sergey Yelenski <sergey.yelenski@warecorp.com>
2020-12-23 15:59:28 +0300

    new version added

--- app/controllers/cc_addresses_controller.rb
+++ app/controllers/cc_addresses_controller.rb
@@ -1,50 +1,39 @@
 class CcAddressesController < ApplicationController
-  before_filter :find_project, :except => [ :new_issue_add_cc ]
-  before_filter :authorize, :except => [ :new_issue_add_cc ]
+  before_action :find_project
+  before_action :authorize
   unloadable
 
   def create
-    @cc_address = CcAddress.new(params[:new_address])
-    respond_to do |format|
-      if @cc_address.save
-        format.html { redirect_to :controller => 'issues', :action => 'show', :id => @issue }
-        format.js do
-          render :update do |page|
-            if @cc_address.errors.empty?
-              @cc_address = nil
-            end
-            page.replace_html "cc-addresses", :partial => 'issues/cc_addresses', :locals => { :issue => @issue, :project => @project }
-          end
-        end
-      else
-        format.html { redirect_to :controller => 'issues', :action => 'show', :id => @issue }
-        format.js do
-          render :update do |page|
-            page.replace_html "cc-addresses", :partial => 'issues/cc_addresses', :locals => { :issue => @issue, :project => @project }
-          end
-        end
+    @cc_address ||= CcAddress.new(addr_params)
+    if request.post? && @cc_address.save
+      respond_to do |format|
+        format.html { redirect_to :back }
+        format.js
       end
     end
   end
 
   def destroy
-    cc_address = CcAddress.find(params[:id])
-    if request.post? && @issue.cc_addresses.include?(cc_address)
-      cc_address.destroy
-      @issue.reload
+    @cc_address = CcAddress.find(params[:id])
+    if request.post? && @issue.cc_addresses.include?(@cc_address)
+      @cc_address.destroy
     end
-
     respond_to do |format|
-      format.html { redirect_to :controller => 'issues', :action => 'show', :id => @issue }
-      format.js { render(:update) { |page| page.replace_html "cc-addresses", :partial => 'issues/cc_addresses', :locals => { :issue => @issue, :project => @project } } }
+      format.html { redirect_to :back }
+      format.js
     end
   end
-
+  
 private
+  def addr_params
+    params.require(:new_address).permit(:mail, :issue_id)
+  end
+
   def find_project
     @issue = Issue.find(params[:issue_id])
     @project = @issue.project
   rescue ActiveRecord::RecordNotFound
     render_404
   end
+
 end
--- init.rb
+++ init.rb
@@ -1,50 +1,27 @@
-require 'redmine'
-
-# Patches to the Redmine core.
-require 'dispatcher'
-
-ActionDispatch::Callbacks.to_prepare do
-  require_dependency 'cc_addresses_issue_show_hook'
-  require_dependency 'redmine_cc_addresses/hooks/view_issues_edit_notes_bottom_hook'
-  require_dependency 'redmine_cc_addresses/hooks/controller_issues_edit_before_save_hook'
-  require_dependency 'redmine_cc_addresses/hooks/view_issues_history_journal_bottom_hook'
-
-  require_dependency 'issue'
-  # Guards against including the module multiple time (like in tests)
-  # and registering multiple callbacks
-  unless Issue.included_modules.include? RedmineCcAddresses::IssuePatch
-    Issue.send(:include, RedmineCcAddresses::IssuePatch)
-  end
-
-  require_dependency 'mailer'
-  unless Mailer.included_modules.include? RedmineCcAddresses::MailerPatch
-    Mailer.send(:include, RedmineCcAddresses::MailerPatch)
-  end
-
-  require_dependency 'mail_handler'
-  unless MailHandler.included_modules.include? RedmineCcAddresses::MailHandlerPatch
-    MailHandler.send(:include, RedmineCcAddresses::MailHandlerPatch)
-  end
-
-  config_file = Rails.root + '/config/tickets.yml'
-  if File.file?(config_file)
-    config = YAML::load_file(config_file)
-    if config.is_a?(Hash) && config.has_key?(Rails.env)
-      TicketMailer.new_ticket_headers = config[Rails.env]['new_ticket_headers']
-    end
-  end
-
-end
-
 Redmine::Plugin.register :redmine_cc_addresses do
-  name 'Issue CC Addresses'
+  name 'Issue CC addresses'
   author 'Nick Peelman'
-  description 'Allows CC Addresses to be attached to an issue'
-  version '0.2.0'
+  author_url 'http://peelman.us'
+  url 'https://github.com/ageis/redmine_cc_addresses'
+  description 'Allows CC addresses to be attached to an issue'
+  version '0.4.0'
 
   project_module :cc_addresses do |map|
-    map.permission :view_cc_addresses, { }
-    map.permission :add_cc_addresses, { :cc_addresses => :create }
-    map.permission :delete_cc_addresses, { :cc_addresses => :destroy }
+    map.permission :edit_cc_addresses, { :cc_addresses => [:create, :destroy] }
+  end
+end
+
+require_dependency 'issue_patch'
+require_dependency 'mailer_patch'
+
+ActionDispatch::Callbacks.to_param do
+  require_dependency 'cc_addresses_issue_show_hook'
+  require_dependency 'issue'
+  unless Issue.included_modules.include? IssuePatch
+    Issue.send(:include, IssuePatch)
+  end
+  require_dependency 'mailer'
+  unless Mailer.included_modules.include? MailerPatch
+    Mailer.send(:include, MailerPatch)
   end
 end
